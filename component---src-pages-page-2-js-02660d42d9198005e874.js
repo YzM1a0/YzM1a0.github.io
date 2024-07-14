795593e2aaf795237ea566654336bd5fd109fe92
YzM1a0 <2694331075@qq.com>
2024-07-14 18:41:12 +0800

    Updates

--- component---src-pages-page-2-js-02660d42d9198005e874.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[617],{4948:function(e,t,a){a.r(t),a.d(t,{Head:function(){return u}});var l=a(7294),n=a(574),r=a(5592),c=a(8183);const u=()=>l.createElement(c.Z,{title:"Page two"});t.default=()=>l.createElement(r.Z,null,l.createElement("h1",null,"Hi from the second page"),l.createElement("p",null,"Welcome to page 2"),l.createElement(n.Link,{to:"/"},"Go back to the homepage"))}}]);
-//# sourceMappingURL=component---src-pages-page-2-js-02660d42d9198005e874.js.map
